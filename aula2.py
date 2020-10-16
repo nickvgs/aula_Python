680ba8c2d69641c7481e4c7bb45c0bb632b824db
Nicolas <nick_.s@hotmail.com>
2020-10-15 21:07:35 -0300

    aula de python com PyCharm

---
+++ aula2.py
@@ -0,0 +1,22 @@
+a = 10
+b = 6
+soma = a + b
+#print(soma)
+subtracao = a - b
+multiplicao = a * b
+divisao = a/b
+resto = a%b
+print("soma: {}" .format(soma)) #maneira correta para concatenar sem necessidade de conversão
+print("soma: {}. Subtração {}" .format (soma, subtracao)) #declarar na ordem
+print(subtracao)
+print(multiplicao)
+print(divisao)
+print(resto)
+print (type(soma)) #verifica o tipo da váriavel
+soma = str(soma)
+print (type(soma))
+print ("soma: " + soma)
+
+x = 1
+soma
+
---
+++ meu_app.py
@@ -0,0 +1,6 @@
+print("Meu primeiro programa python")
+
+A = 20
+B = 10
+soma = A+B
+print (soma)
