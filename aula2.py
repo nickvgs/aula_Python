a2a4f842355376e8944d6e266c7ca0f8f88b47de
Nicolas <nick_.s@hotmail.com>
2020-10-16 23:27:30 -0300

    aula 2 de python, print, variavel, quebra de linha, etc

--- aula2.py
+++ aula2.py
@@ -7,7 +7,7 @@ multiplicao = a * b
 divisao = a/b
 resto = a%b
 print("soma: {}" .format(soma)) #maneira correta para concatenar sem necessidade de conversão
-print("soma: {}. Subtração {}" .format (soma, subtracao)) #declarar na ordem
+print("soma: {soma}. \nSubtração: {subtracao}." .format (soma=soma, subtracao=subtracao)) #declarar na ordem  e \n quebra de linha
 print(subtracao)
 print(multiplicao)
 print(divisao)
@@ -15,8 +15,22 @@ print(resto)
 print (type(soma)) #verifica o tipo da váriavel
 soma = str(soma)
 print (type(soma))
-print ("soma: " + soma)
+print ("soma:" + soma)
 
 x = 1
 soma
 
+l= int(input("Entre com o primeiro numero "))
+m= int(input("entre com o segundo valor"))
+soma = (l + m)
+
+print (soma)
+
+int(soma)
+
+brigadeiros = 150
+pessoas = 16
+
+resultado = int(brigadeiros/pessoas)
+
+print (resultado)
